d483eace50dbc00eb2e67870c8dc5fe8aaaeeef2
Alen Rubilar <lclc.alen@gmail.com>
2025-02-18 21:08:53 -0300

    feat: add context to constants and update LLM generation

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,48 @@
 export const NOTION_PROJECTS_URL = "https://000alen.notion.site/Projects-19ede7d2d0f78030b0a2e43b3ba96391";
+
+export const CONTEXT = `\
+Alen Rubilar
+alen@uni.minerva.edu | https://www.linkedin.com/in/000alen/ | https://github.com/000alen/
+
+SKILLS
+C++, Python, Julia, Software Engineering, Scientific Computing, Machine Learning, Natural Language Understanding, Computer Vision, PyTorch, Tensorflow, ONNX, Data Structures, Group Theory, Topology
+
+EDUCATION
+B.Sc. Computer Science, Mathematics @ Minerva University	2022 – 2026, San Francisco, United States
+B.Sc. Mathematical Engineering @ Pontificia Universidad Católica de Chile	2021, Santiago, Chile
+Advanced Computer Science @ Universidad Técnica Federico Santa María	2019 – 2020, Santiago, Chile
+
+EXPERIENCE
+Meiler Lab @ Vanderbilt University	Remote
+Deep Learning Research Intern	January 2024 — March 2024
+Explored architectural improvements for Diffusion NNs for protein folding (RoseTTAFold diffusion)
+Fine-tuned NNs for downstream tasks (predicting protein-ligand binding affinity)
+Meiler Lab @ Leipzig University	Leipzig, Germany
+Deep Learning Research Intern	July 2023 — August 2023
+Trained Topological NNs for molecule optimization (binding affinity)
+Proposed a method for molecule data augmentation with Grammar-Constrained Generative NNs
+Developed an autonomous Language Agent for protein thermostabilization
+DataRoot	Remote
+Founder Machine Learning Engineer	April 2022—present
+Designed lightweight disease and defect detection model (Convolutional NNs) for produce quality control (deployed in U.S. fruit importers). Reduced inspection from 2 hours to 15 minutes
+Designed an automated platform for low-code data-driven applications
+Reduce time-to-first-prototype from 2 weeks to 2 hours
+
+AI/ML CHALLENGES & HACKATHONS
+Concurrent Agent Executor	August 2023, Leipzig, Germany
+Designed a topological task runner with speculative execution capabilities for Program-Augmented Language Models with Improvements over arxiv:2210.03629
+Wellytics (Generative AI, AI Agents, NLP, LLMs, Transformers)	April 2023, San Francisco, CA
+Serverless surveying platform powered by Natural Language Understanding for qualitative analysis
+Reduce time-to-feedback from 3 months to 5 seconds (real-time)
+Kairos (Generative AI, AI Agents, NLP, LLMs, Transformers)	February 2023, Palo Alto, United States
+Research project replicating results from Schick et al. (2023) by teaching Mid-Sized Language Models how to use external tools and applying it to practical contexts such as real-time meeting transcription
+Made during TreeHacks
+PulseBud (RNNs, LSTMs, real-time, edge-computing)	October 2022, San Francisco, United States
+Real-time seizure prediction and emergency notification system; achieved accuracy of 92%. Modified Long-Short Term Memory NNs architecture for continuous inference with a stream of data
+Won CalHacks and several partner challenges, out of 211 projects submitted
+Phaedra (Generative AI, NLP, Semantic Search, LLMs)	September 2021, Boston, United States
+Reading assistant for summarization, key-phrase extraction, and question answering. Designed semantic search algorithm for abstract question answering with Mid-Sized Language Models (T5)
+Won HackMIT’s Inclusive Social Networks challenge, out of 146 projects submitted
+TótemUV (Computer Vision, CNNs, UV Imaging)	December 2020, Santiago, Chile
+Tool to see invisible skin damage due to overexposure to UV radiation with Convolutional NNs to detect skin cancer
+Won Solve for Tomorrow, out of 334 projects submitted`
--- lib/llm.ts
+++ lib/llm.ts
@@ -1,6 +1,6 @@
-import fs from "fs";
 import { CoreMessage } from "ai";
 import { openai } from "@ai-sdk/openai";
+import { CONTEXT } from "@/lib/constants";
 
 // export const languageModel = openai("o3-mini");
 export const languageModel = openai("gpt-4o-mini");
@@ -8,12 +8,10 @@ export const languageModel = openai("gpt-4o-mini");
 export const embeddingModel = openai.embedding("text-embedding-3-small");
 
 export const getMessages = async (query: string): Promise<CoreMessage[]> => {
-  const context = await fs.promises.readFile("context.txt", "utf8");
-
   return [
     {
       role: "system",
-      content: `You are tasked to created a tailored introduction for Alen Rubilar. The introduction should be tailored to a specific query. The introduction should be concise and to the point. The introduction should be no more than 100 words. This is the context of Alen Rubilar:\n\n"""\n${context}\n"""`,
+      content: `You are tasked to created a tailored introduction for Alen Rubilar. The introduction should be tailored to a specific query. The introduction should be concise and to the point. The introduction should be no more than 100 words. This is the context of Alen Rubilar:\n\n"""\n${CONTEXT}\n"""`,
     },
     {
       role: "user",
