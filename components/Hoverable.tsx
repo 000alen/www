f241103e46fbfac97a89b1c4f07bc6827eda0529
Alen Rubilar <lclc.alen@gmail.com>
2025-02-21 02:03:09 -0300

    feat: improve Hoverable component with dark mode support

--- components/Hoverable.tsx
+++ components/Hoverable.tsx
@@ -16,10 +16,10 @@ export default function Hoverable({ title, description, children }: HoverablePro
       <HoverCardTrigger asChild>
         {children}
       </HoverCardTrigger>
-      <HoverCardContent className="w-80 bg-[#1E1E1E] border-[#333333] p-4 text-white">
+      <HoverCardContent className="w-80 p-4 bg-[#1E1E1E] border-[#333333] text-white dark:bg-white dark:border-[#333333] dark:text-black">
         <div className="space-y-2">
           <div className="text-sm font-medium">{title}</div>
-          <div className="text-xs text-[#909090]">
+          <div className="text-xs">
             {description || "Click to open in a new tab"}
           </div>
         </div>
